7b5fb32039e4c3c618643e6d35b246c32b62ed5e
Bright Anning <anningbright578@gmail.com>
2026-03-15 03:02:12 +0000

    server setup, folder structure and database configuration

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/db.js';
+
+
+
+
+
+
+const app = express();
+app.use(cors({
+    origin: "*",
+    
+}));
+app.use(express.json());
+
+
+//routes
+
+
+
+
+const PORT = process.env.PORT ;
+connectDB().then (()=>{
+    app.listen(PORT, () => {
+    console.log(`Server is running on port`);
+    console.log("DB connected");
+    
+});
+})
